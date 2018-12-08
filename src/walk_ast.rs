1d7ec3cf75ad75351611752735ee0dda0e2f16a2
David Pedersen <david.pdrsn@gmail.com>
2018-12-08 11:07:10 +0100

    Pull juniper code generation into separate module

--- src/walk_ast.rs
+++ src/walk_ast.rs
@@ -1,3 +1,5 @@
 mod find_special_scalar_types;
+mod gen_juniper_code;
 
 pub use self::find_special_scalar_types::{find_special_scalar_types, SpecialScalarTypesList};
+pub use self::gen_juniper_code::{gen_doc, Output};
